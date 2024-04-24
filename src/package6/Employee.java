13642d651ade32d353d9e4c6d238b113138e59ca
Adeoluwa Idowu <adeoluwa53@gmail.com>
2024-04-24 10:28:56 +0100

    inheritance2

---
+++ src/package6/Employee.java
@@ -0,0 +1,11 @@
+package package6;
+
+public class Employee extends Person{
+    public static void main ( String args[]){
+        Person person = new Person();
+    };
+    public String employeeTitle;
+    public int employeeId;
+
+
+}
