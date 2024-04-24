5b64ea5d79f61b39586eadddd81d82b810780b43
Adeoluwa Idowu <adeoluwa53@gmail.com>
2024-04-24 10:28:31 +0100

    inheritance 2

---
+++ src/package6/Cake.java
@@ -0,0 +1,14 @@
+package package6;
+
+public class Cake {
+    String flavor;
+    double price;
+
+    public double getPrice (){
+        return price;
+    }
+
+    public void setPrice( double price){
+        this.price = price;
+    }
+}
